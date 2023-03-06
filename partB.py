f4e157e8566455a14ff009a411561cb6333e2db8
FrankC4231 <124303410+FrankC4231@users.noreply.github.com>
2023-03-06 10:53:10 -0800

    Part b

---
+++ partB.py
@@ -0,0 +1,16 @@
+
+#input a number 
+n = input("Enter a number: ")
+n = int(n)
+
+#prime number check 
+if n > 1:
+	for i in range(2, int(n/2)+1):
+		if (n % i) == 0:
+			print(n, "is not a prime number")
+		break
+	else:
+		print(n, "is a prime number")
+else:
+	print(n, "is not a prime number")
+	
